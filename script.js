c7b410e9d68235a87d17728c7e1eddc2714eb939
Dinusha64 <121313060+Dinusha64@users.noreply.github.com>
2025-07-27 19:06:33 +0530

    Change about and remove projects

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 //typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Software Application Support', 'Fullstack Developer', 'UI/UX Developer'],
+    strings: ['HR and Compliance', 'Support and Implementation Engineer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
